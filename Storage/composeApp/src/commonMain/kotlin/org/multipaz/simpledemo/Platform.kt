7f414c7fbe9cefb55d545686488474b1af83d77e
Hanlu Feng <hanlu@openmobilehub.com>
2025-06-30 15:52:03 -0700

    remove Storage

--- Storage/composeApp/src/commonMain/kotlin/org/multipaz/simpledemo/Platform.kt
+++
@@ -1,7 +0,0 @@
-package org.multipaz.simpledemo
-
-interface Platform {
-    val name: String
-}
-
-expect fun getPlatform(): Platform
--- Storage/composeApp/src/iosMain/kotlin/org/multipaz/simpledemo/MainViewController.kt
+++
@@ -1,5 +0,0 @@
-package org.multipaz.simpledemo
-
-import androidx.compose.ui.window.ComposeUIViewController
-
-fun MainViewController() = ComposeUIViewController { App() }
