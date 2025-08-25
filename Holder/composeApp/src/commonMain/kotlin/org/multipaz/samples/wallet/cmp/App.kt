89b06509e9af7f4b1d01137f4a0adccbac149d9c
Hanlu Feng <hanlu@openmobilehub.com>
2025-08-25 11:26:34 -0700

    feat: chang display name of cert

--- Holder/composeApp/src/commonMain/kotlin/org/multipaz/samples/wallet/cmp/App.kt
+++ Holder/composeApp/src/commonMain/kotlin/org/multipaz/samples/wallet/cmp/App.kt
@@ -192,7 +192,7 @@ class App() {
                         certificate = X509Cert.fromPem(
                             getReaderRootCert().trimIndent().trim()
                         ),
-                        displayName = "OWF Multipaz Reader App",
+                        displayName = "Multipaz Identity Reader (Trusted Devices)",
                         displayIcon = null,
                         privacyPolicyUrl = "https://apps.multipaz.org"
                     )
@@ -202,7 +202,7 @@ class App() {
                         certificate = X509Cert.fromPem(
                             getReaderRootCertForUntrustDevice().trimIndent().trim()
                         ),
-                        displayName = "OWF Multipaz Reader App",
+                        displayName = "Multipaz Identity Reader (UnTrusted Devices)",
                         displayIcon = null,
                         privacyPolicyUrl = "https://apps.multipaz.org"
                     )
